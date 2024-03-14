6979123d3e97369c2b0d783a9af63a8ff268fc19
Sergio Barreras <sergiobg.isc@gmail.com>
2024-03-14 15:12:47 -0700

    problemas solucionados, creo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { ParticipantesModule } from './participantes/participantes.module';
+
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true,
+      migrationsRun: true,
+    }),
+    ParticipantesModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
